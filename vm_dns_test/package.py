c6862f6f4575b8396198b88bfcde06da0a9996d9
shadeofblue <blue@golem.network>
2024-01-12 13:32:11 +0100

    the urls list can be empty ;)

--- vm_dns_test/package.py
+++ vm_dns_test/package.py
@@ -13,7 +13,7 @@ async def get_package():
         DEFAULT_REPOSITORY_URL, image_tag=IMAGE_TAG
     )
     img_hash = resolved_package.split(":")[2]
-    manifest_obj = await Manifest.generate(img_hash, ["https://pypi.dev.golem.network"])
+    manifest_obj = await Manifest.generate(img_hash, [])
     manifest = json.dumps(manifest_obj.dict(by_alias=True))
     encoded_manifest = base64.b64encode(manifest.encode("utf-8")).decode("ascii")
     return await vm.manifest(encoded_manifest, capabilities=["vpn", "inet", "manifest-support"])
